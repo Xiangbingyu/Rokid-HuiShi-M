38d601ddfb3c2d418055c1c296054456d38377a4
Xiangbingyu <2473679079@qq.com>
2025-12-17 20:09:13 +0800

    feat: 重构项目基础结构，实现蓝牙连接

---
+++ huishi-m2/src/main/java/com/demo/huishi_m2/dataBeans/CONSTANT.kt
@@ -0,0 +1,8 @@
+package com.demo.huishim.dataBeans
+
+import com.demo.huishim.R
+
+object CONSTANT {
+    const val CLIENT_SECRET = "b04839a8-d740-11f0-961e-043f72fdb9c8"
+    fun getSNResource() = R.raw.rokid
+}
